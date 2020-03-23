d94797d0f1f08a24c50365b91002c08f66260119
Mohit Kadel <er.mohitkadel@gmail.com>
2020-03-23 18:00:57 +0530

    Updating server.js file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const _ = require('lodash');
 const app = express();
+const bodyParser = require('body-parser');
 
 const dataSet = [
 	{
